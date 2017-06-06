9d623a808c42338a8d176f11daefa47170c9c797
JackDaviesWork <jackdavieswork@hotmail.co.uk>
2017-06-06 10:15:25 +0100

    Enable SVG files to be uploaded

--- functions.php
+++ functions.php
@@ -369,5 +369,38 @@ function register_my_custom_menu_page() {
 }
 add_action( 'admin_menu', 'register_my_custom_menu_page' );
 
+// Allow SVG files to be uploaded to the media library
+add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
+
+  global $wp_version;
+  if ( $wp_version !== '4.7.1' ) {
+     return $data;
+  }
+
+  $filetype = wp_check_filetype( $filename, $mimes );
+
+  return [
+      'ext'             => $filetype['ext'],
+      'type'            => $filetype['type'],
+      'proper_filename' => $data['proper_filename']
+  ];
+
+}, 10, 4 );
+
+function cc_mime_types( $mimes ){
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter( 'upload_mimes', 'cc_mime_types' );
+
+function fix_svg() {
+  echo '<style type="text/css">
+        .attachment-266x266, .thumbnail img {
+             width: 100% !important;
+             height: auto !important;
+        }
+        </style>';
+}
+add_action( 'admin_head', 'fix_svg' );
 
 ?>
