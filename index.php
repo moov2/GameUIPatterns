ce3208b39a792ddf72365417dfdd245c6f5fb4a0
JackDaviesWork <jackdavieswork@hotmail.co.uk>
2017-06-06 12:37:22 +0100

    Query for custom post type

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
 
 			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
 
+			<?php
+
+			$args = array('post_type'=>array('posts', 'gameui'));
+
+		    query_posts($args);
+
+			?>
+
 			<?php get_template_part('loop'); ?>
 
 			<?php get_template_part('pagination'); ?>
