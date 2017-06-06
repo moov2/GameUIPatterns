e0b548c3bb5af33c686ddaea2b4848fc8b904f2b
JackDaviesWork <jackdavieswork@hotmail.co.uk>
2017-06-06 12:37:04 +0100

    Add footer copyright to header

--- footer.php
+++ footer.php
@@ -1,14 +1,4 @@
-			<!-- footer -->
-			<footer class="footer" role="contentinfo">
 
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>.
-				</p>
-				<!-- /copyright -->
-
-			</footer>
-			<!-- /footer -->
 
 		</div>
 		<!-- /wrapper -->
--- header.php
+++ header.php
@@ -39,6 +39,18 @@
 			</header>
 			<!-- /header -->
 
+			<!-- footer -->
+			<footer class="footer" role="contentinfo">
+
+				<!-- copyright -->
+				<p class="copyright">
+					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>.
+				</p>
+				<!-- /copyright -->
+
+			</footer>
+			<!-- /footer -->
+
 		</aside>
 
 		<!-- wrapper -->
