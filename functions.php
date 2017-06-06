c113b38e0502f16c613aa72f583fb058ec5b5faf
JackDaviesWork <jackdavieswork@hotmail.co.uk>
2017-06-06 10:44:44 +0100

    Update functions to import minified CSS file

--- functions.php
+++ functions.php
@@ -90,7 +90,7 @@ function gameui_nav()
 // Load GameUI styles
 function gameui_styles()
 {
-    wp_register_style('gameui', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
+    wp_register_style('gameui', get_template_directory_uri() . '/style.min.css', array(), '1.0', 'all');
     wp_enqueue_style('gameui'); // Enqueue it!
 }
 
