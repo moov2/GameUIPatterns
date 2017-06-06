43ac04b14aaa4449fe88bc6f552c178e5df97ee4
JackDaviesWork <jackdavieswork@hotmail.co.uk>
2017-06-06 10:18:58 +0100

    Remove wordpress tagline at dashboard

--- functions.php
+++ functions.php
@@ -403,4 +403,16 @@ function fix_svg() {
 }
 add_action( 'admin_head', 'fix_svg' );
 
+// Remove Wordpress in footer of admin dashboard
+
+function wpse_edit_footer() {
+    add_filter( 'admin_footer_text', 'wpse_edit_text', 11 );
+}
+
+function wpse_edit_text($content) {
+    return "";
+}
+
+add_action( 'admin_init', 'wpse_edit_footer' );
+
 ?>
