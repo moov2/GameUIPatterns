548116479de3606f6659e24911bcdc511dd26add
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-08 21:16:21 +0100

    Call ACF plugin fields

--- single-gameui.php
+++ single-gameui.php
@@ -1,65 +1,78 @@
 <?php get_header(); ?>
 
-	<main role="main">
+<main role="main">
 	<!-- section -->
 	<section>
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<!-- article -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
+				<h3><?php the_category(); ?> /</h3>
 
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
+				<!-- post title -->
+				<h1>
+					<?php the_title(); ?>
+				</h1>
+				<!-- /post title -->
 
-			<!-- post details -->
-			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-			<!-- /post details -->
+				<h2>Description</h2>
 
-			<?php the_content(); // Dynamic Content ?>
+				<?php
+				if(get_field('description'))
+				{
+					echo get_field('description');
+				}
 
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+				the_post_thumbnail();
 
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
+				echo "<h2>When to use</h2>";
 
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
+				if(get_field('when_to_use'))
+				{
+					echo get_field('when_to_use');
+				}
 
-			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
+				echo "<h2>Solution</h2>";
 
-			<?php comments_template(); ?>
+				if(get_field('solution'))
+				{
+					echo get_field('solution');
+				}
 
-		</article>
-		<!-- /article -->
+				get_template_part('partials/gameui', 'examples');
 
-	<?php endwhile; ?>
+				echo "<h2>Technical Details</h2>";
 
-	<?php else: ?>
+				if(get_field('technical_details'))
+				{
+					echo get_field('technical_details');
+				}
 
-		<!-- article -->
-		<article>
+				comments_template();
 
-			<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
+				?>
 
-		</article>
-		<!-- /article -->
+				</article>
+				<!-- /article -->
 
-	<?php endif; ?>
+			<?php endwhile; ?>
+
+		<?php else: ?>
+
+			<!-- article -->
+			<article>
+
+				<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
+
+			</article>
+			<!-- /article -->
+
+		<?php endif; ?>
 
 	</section>
 	<!-- /section -->
-	</main>
+</main>
 
 <?php get_footer(); ?>
