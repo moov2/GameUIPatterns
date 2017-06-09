5946be92341dad1d07706ab1dc435f90a618a66b
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-09 12:17:41 +0100

    Fix permalinks to hide custom post type

--- functions.php
+++ functions.php
@@ -484,4 +484,42 @@ function at_glance_content_ui_patterns() {
     }
 }
 
+/**
+ * Remove the slug from published post permalinks. Only affect our custom post type, though.
+ */
+function gp_remove_cpt_slug( $post_link, $post, $leavename ) {
+
+    if ( 'gameui' != $post->post_type || 'publish' != $post->post_status ) {
+        return $post_link;
+    }
+
+    $post_link = str_replace( '/' . $post->post_type . '/', '/', $post_link );
+
+    return $post_link;
+}
+add_filter( 'post_type_link', 'gp_remove_cpt_slug', 10, 3 );
+
+/**
+ * Have WordPress match postname to any of our public post types (post, page, gameui)
+ * All of our public post types can have /post-name/ as the slug, so they better be unique across all posts
+ * By default, core only accounts for posts and pages where the slug is /post-name/
+ */
+function gp_parse_request_trick( $query ) {
+
+    // Only noop the main query
+    if ( ! $query->is_main_query() )
+        return;
+
+    // Only noop our very specific rewrite rule match
+    if ( 2 != count( $query->query ) || ! isset( $query->query['page'] ) ) {
+        return;
+    }
+
+    // 'name' will be set if post permalinks are just post_name, otherwise the page rule will match
+    if ( ! empty( $query->query['name'] ) ) {
+        $query->set( 'post_type', array( 'post', 'page', 'gameui' ) );
+    }
+}
+add_action( 'pre_get_posts', 'gp_parse_request_trick' );
+
 ?>
