9390c9bbf19a389d53175d2c751d538c0060baaa
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-12 10:06:51 +0100

    Echo out voting plugin statistics

--- partials/gameui-examples.php
+++ partials/gameui-examples.php
@@ -22,6 +22,7 @@ if ( $connected->have_posts() ) :
                     {
                         echo get_field('display_title');
                     }?>
+                    <?=function_exists('thumbs_rating_getlink') ? thumbs_rating_getlink() : ''?>
                 </li>
             <?php endwhile; ?>
         </ul>
