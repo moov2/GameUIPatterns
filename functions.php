2fddecaeca02a561f54abff0df76c29e17cc6be0
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-12 14:19:38 +0100

    Create custom wrapper image sizes

--- functions.php
+++ functions.php
@@ -27,6 +27,8 @@ if (function_exists('add_theme_support'))
 
     // Add Thumbnail Theme Support
     add_theme_support('post-thumbnails');
+    add_image_size('wrapper', 850, '', true);
+    add_image_size('wrapper-full', 1560, '', true);
     add_image_size('large', 700, '', true); // Large Thumbnail
     add_image_size('medium', 250, '', true); // Medium Thumbnail
     add_image_size('small', 120, '', true); // Small Thumbnail
