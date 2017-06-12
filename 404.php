ae6d827e57f40914d7d2a38382eb3c6969c457dd
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-12 14:11:31 +0100

    Update 404 page with new format

--- 404.php
+++ 404.php
@@ -2,18 +2,16 @@
 
 	<main role="main">
 		<!-- section -->
-		<section>
+		<section class="section padding--top-large">
 
-			<!-- article -->
-			<article id="post-404">
+			<div class="section__content">
 
 				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
 				<h2>
 					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
 				</h2>
 
-			</article>
-			<!-- /article -->
+			</div>
 
 		</section>
 		<!-- /section -->
