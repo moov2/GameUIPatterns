b31918f3468490392c4f6f39c8efb4a4e1f72c45
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-12 12:17:48 +0100

    Include the page content inside of the section__content class

--- page.php
+++ page.php
@@ -13,16 +13,15 @@
 
 						<h1><?php the_title(); ?></h1>
 
+						<?php the_content(); ?>
+
+						<?php comments_template( '', true ); // Remove if you don't want comments ?>
+
+						<br class="clear">
+
+						<?php edit_post_link(); ?>
 					</div>
 				</section>
-
-				<?php the_content(); ?>
-
-				<?php comments_template( '', true ); // Remove if you don't want comments ?>
-
-				<br class="clear">
-
-				<?php edit_post_link(); ?>
 
 			</article>
 			<!-- /article -->
