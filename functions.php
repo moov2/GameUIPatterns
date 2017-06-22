82c69c0e604e8fa31338095195028f8dee0f4e66
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-22 14:32:41 +0100

    Add custom HTML callback for comments

--- functions.php
+++ functions.php
@@ -441,6 +441,37 @@ function at_glance_content_ui_patterns() {
 }
 
 
+
+function format_comment($comment, $args, $depth) {
+    $GLOBALS['comment'] = $comment; ?>
+
+    <li class="comment list__item margin--bottom-default">
+
+        <?php echo get_avatar( $current_user->user_email, 64, null, null, array('class' => array('comment__avatar', 'border-radius--circle') ) ); ?>
+
+        <div class="comment__meta text--grey text--small">
+            Posted: <?php printf(__('%1$s'), get_comment_date(), get_comment_time()) ?> by <?php printf(__('%s'), get_comment_author_link()) ?>
+        </div>
+
+        <?php if ($comment->comment_approved == '0') : ?>
+            <php _e('Your comment is awaiting moderation.') ?><br />
+        <?php endif; ?>
+
+        <?php comment_text(); ?>
+
+        <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+
+    </li>
+
+<?php }
+// filter to replace class on reply link
+add_filter('comment_reply_link', 'replace_reply_link_class');
+function replace_reply_link_class($class){
+    $class = str_replace("class='comment-reply-link", "class='comment__reply text--bold", $class);
+    return $class;
+}
+
+
 /**
  * This file represents an example of the code that themes would use to register
  * the required plugins.
