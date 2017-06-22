694579c68554140cfa69db8ad2b2d4ca868ebaa0
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-22 13:04:14 +0100

    Call W3TC javascript in footer of website

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 
 		<?php wp_footer(); ?>
 
+		<!-- W3TC-include-js-head -->
+
 		<script>
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
