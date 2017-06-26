0ae3b0262ccdb4b9571ce25a11937c79ae32a855
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-26 15:28:26 +0100

    Create newsletter partial

---
+++ partials/newsletter.php
@@ -0,0 +1,9 @@
+<div class="newsletter">
+    <h4>Newsletter Signup</h4>
+    <h3>Game UI patterns. Once a month. No spam.</h3>
+    <p>Get monthly updates and articles from guest writers in the games industry.<p>
+    <form>
+        <input type="text">Enter your email address</input>
+        <button type="submit">Submit</button>
+    </form>
+</div>
