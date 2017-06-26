e953ba76e0d8172887cfa76aac7547d3fdb504cf
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-26 19:35:39 +0100

    Add section classes to GameUI 12 Component

--- partials/gameui-twelve.php
+++ partials/gameui-twelve.php
@@ -8,34 +8,36 @@ $connected = new WP_Query( array(
 // Display connected pages
 if ( $connected->have_posts() ) :
     ?>
-    <section class="padding--default">
-        <h3 class="padding--horizontal-small">Latest UI Patterns</h3>
-        <div class="column-container column-container--grow column-size--4">
-            <?php while ( $connected->have_posts() ) : $connected->the_post(); ?>
-                <div class="column padding--small flex flex--direction-column flex--grow">
-                    <h4 class="margin--bottom-tiny"><?php the_title(); ?></h4>
+    <section class="section section--wide">
+        <div class="section__content padding--small">
+            <h2 class="padding--horizontal-small">Latest UI Patterns</h2>
+            <div class="column-container column-container--grow column-size--4">
+                <?php while ( $connected->have_posts() ) : $connected->the_post(); ?>
+                    <div class="column padding--small flex flex--direction-column flex--grow margin--bottom-default">
+                        <h4 class="margin--bottom-tiny"><?php the_title(); ?></h4>
 
-                    <?php
-                    $categories = get_the_category();
-                    if ( ! empty( $categories ) ) {
-                        echo '<h5 class="text--grey">&#187; ';
-                        echo esc_html( $categories[0]->name );
-                        echo '</h5>';
-                    }
-                    ?>
+                        <?php
+                        $categories = get_the_category();
+                        if ( ! empty( $categories ) ) {
+                            echo '<h5 class="text--grey">&#187; ';
+                            echo esc_html( $categories[0]->name );
+                            echo '</h5>';
+                        }
+                        ?>
 
-                    <?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-                        <?php the_post_thumbnail('pattern__thumbnail', ['class' => 'pattern__thumbnail', 'title' => 'Feature image']); // Fullsize image for the single post ?>
-                    <?php endif; ?>
-                    <?php $meta = get_post_meta(get_the_ID(), '_yoast_wpseo_metadesc', true); ?>
-                    <?php if($meta) {?>
-                        <p><?php echo $meta;?></p>
-                    <?php } ?>
-                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="text--orange text--chevron text--small text--bold">Read more </a>
+                        <?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+                            <?php the_post_thumbnail('pattern__thumbnail', ['class' => 'ui__thumbnail margin--bottom-default', 'title' => 'Feature image']); // Fullsize image for the single post ?>
+                        <?php endif; ?>
+                        <?php $meta = get_post_meta(get_the_ID(), '_yoast_wpseo_metadesc', true); ?>
+                        <?php if($meta) {?>
+                            <p><?php echo $meta;?></p>
+                        <?php } ?>
+                        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="text--orange text--chevron text--small text--bold">Read more </a>
 
-                    <hr class="hr hr--grey-light">
-                </div>
-            <?php endwhile; ?>
+                        <hr class="hr hr--grey-light">
+                    </div>
+                <?php endwhile; ?>
+            </div>
         </div>
     </section>
     <?php
