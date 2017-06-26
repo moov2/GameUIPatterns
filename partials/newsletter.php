617d86b8b69612ed9aadc790f686b8eb9a0e8287
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-26 16:29:07 +0100

    Complete newsletter partial for mobile

--- partials/newsletter.php
+++ partials/newsletter.php
@@ -1,9 +1,9 @@
 <div class="newsletter padding--small padding--vertical-default bg--color-grey-lightest">
-    <span class="text--grey text--small margin--bottom-small">Newsletter Signup</span>
+    <span class="display--block text--grey text--small margin--bottom-small">Newsletter Signup</span>
     <h3 class="margin--bottom-small">Game UI patterns. Once a month. No spam.</h3>
-    <p class="text--small">Get monthly updates and articles from guest writers in the games industry.<p>
+    <p class="text--small margin--bottom-small">Get monthly updates and articles from guest writers in the games industry.</p>
     <form>
-        <input type="text" placeholder="Enter your email address" class="margin--bottom-small text--white"></input>
-        <button type="submit" class="bg--color-green">Submit</button>
+        <input type="text" placeholder="Enter your email address" class="width--full margin--bottom-small padding--small"></input>
+        <button type="submit" class="width--full cta cta--green">Submit</button>
     </form>
 </div>
