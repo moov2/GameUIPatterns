a91d015abd461a982e0c1ffacd5208de67d42188
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-26 19:55:43 +0100

    Update 404 to include contribute URL

--- 404.php
+++ 404.php
@@ -8,7 +8,7 @@
 				<h1>404</h1>
 				<h2 class="text--grey-dark">We couldn't find this page.</h2>
 
-				<p>Maybe there isn't a page for that pattern yet? You can submit your own pattern using <a href="/submit">this page</a>.</p>
+				<p>Maybe there isn't a page for that pattern yet? You can submit your own pattern using <a href="/contribute">this page</a>.</p>
 
 				<p>You can always checkout our latest patterns at <a href="/">our homepage</a>.</p>
 
