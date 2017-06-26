d32d2e3b2bdaea57d285e2dcf6f38abcb1040a41
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-26 16:17:09 +0100

    Complete mobile build of latest blog post component

--- partials/latest-blog-post.php
+++ partials/latest-blog-post.php
@@ -8,24 +8,26 @@ $connected = new WP_Query( array(
 // Display connected pages
 if ( $connected->have_posts() ) :
     ?>
-    <span class="text-colour--grey">Latest Blog Post</span>
-    <ul class="list flex flex--direction-column flex--justify-between">
+    <div class="padding--small padding--vertical-default">
+        <div class="flex flex--justify-between margin--bottom-small text--small">
+            <span class="text--grey">Latest Blog Post</span>
+            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="text--grey text--chevron">Visit blog </a>
+        </div>
         <?php while ( $connected->have_posts() ) : $connected->the_post(); ?>
-            <li class="pattern__item margin--bottom-default">
-                <div>
-                <?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-                    <?php the_post_thumbnail('pattern__thumbnail', ['class' => 'pattern__thumbnail', 'title' => 'Feature image']); // Fullsize image for the single post ?>
-                <?php endif; ?>
+            <div>
+            <?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+                <?php the_post_thumbnail('pattern__thumbnail', ['class' => 'post__thumbnail margin--bottom-small', 'title' => 'Feature image']); // Fullsize image for the single post ?>
+            <?php endif; ?>
 
-                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><h4><?php the_title(); ?></h4></a>
-                <?php $meta = get_post_meta(get_the_ID(), '_yoast_wpseo_metadesc', true); ?>
-                <?php if($meta) {?>
-                    <p><?php echo $meta;?></p>
-                <?php } ?>
-                </div>
-            </li>
+            <h3 class="margin--bottom-small"><?php the_title(); ?></h3>
+            <?php $meta = get_post_meta(get_the_ID(), '_yoast_wpseo_metadesc', true); ?>
+            <?php if($meta) {?>
+                <p class="margin--bottom-small text--small"><?php echo $meta;?></p>
+            <?php } ?>
+                <a href="<?php the_permalink(); ?>" title="Read more about <?php the_title_attribute(); ?>" class="text--red text--small text--bold text--chevron">Read more </a>
+            </div>
         <?php endwhile; ?>
-    </ul>
+    </div>
     <?php
     // Prevent weirdness
     wp_reset_postdata();
