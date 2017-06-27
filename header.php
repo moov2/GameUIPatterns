a215b56cf8296104ac381a750056b7f4f1194ba8
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-27 14:57:22 +0100

    Display logo at large mobile breakpoint

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
-							<img class="logo__icon margin--bottom-small" src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo">
+							<img class="logo__icon" src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo">
 							<p class="logo__text"><?php printf( get_bloginfo ( 'name' ) ); ?></p>
 						</a>
 					</div>
