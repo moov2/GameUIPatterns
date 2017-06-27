1a1ad323bc12840050642f404dba00446e2c4ff2
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-27 18:05:24 +0100

    Change case sensitivity of theme folder in pattern library link

--- functions.php
+++ functions.php
@@ -375,7 +375,7 @@ remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
 // Link to Pattern Library
 function register_my_custom_menu_page() {
-    add_menu_page( 'GameUIPatterns Pattern Library', 'Pattern Library', 'manage_options', '../wp-content/themes/gameuipatterns/pattern-library/dist/index.html', '', 'dashicons-book', 3 );
+    add_menu_page( 'GameUIPatterns Pattern Library', 'Pattern Library', 'manage_options', '../wp-content/themes/GameUIPatterns/pattern-library/dist/index.html', '', 'dashicons-book', 3 );
 }
 add_action( 'admin_menu', 'register_my_custom_menu_page' );
 
