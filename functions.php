c2622095a9ee0ba411da2c7bddffb3fc52350688
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-27 12:59:26 +0100

    Create unlimited GameUI shortcode

--- functions.php
+++ functions.php
@@ -734,6 +734,15 @@ function twelve_gameui_shortcode() {
 }
 add_shortcode( 'twelve-gameui', 'twelve_gameui_shortcode' );
 
+// Twelve GameUI post types Shortcode
+function unlimited_gameui_paged_shortcode() {
+    ob_start();
+	get_template_part( 'partials/gameui-unlimited' );
+    return ob_get_clean();
+}
+add_shortcode( 'unlimited-gameui', 'unlimited_gameui_paged_shortcode' );
+
+
 // Twelve Posts post types Shortcode
 function twelve_posts_shortcode() {
     ob_start();
