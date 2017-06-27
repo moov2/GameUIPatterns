493b005f4c1d6e0adca2ac54ae67b77ca2329316
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-06-27 13:30:04 +0100

    Add link to category breadcrumb

--- single-gameui.php
+++ single-gameui.php
@@ -15,7 +15,7 @@
 
 						if ( ! empty( $categories ) ) {
 							echo '<p itemscope itemtype="http://schema.org/Article" itemref="_articleSection2 _articleSection3 _articleSection4 _articleBody5" class="padding--horizontal-none@desktop margin--bottom-small text--grey-dark">';
-							echo esc_html( $categories[0]->name );
+							echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
 							echo ' / </p>';
 						}
 					?>
