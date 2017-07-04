f505f77989896a503c5242f53d641cbfdc4c2a20
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-07-04 13:12:37 +0100

    Set a width to SVG logo and prevent harsh loading

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
-							<img class="logo__icon" src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo">
+							<img class="logo__icon" src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" width="100" height="47">
 							<p class="logo__text"><?php printf( get_bloginfo ( 'name' ) ); ?></p>
 						</a>
 					</div>
