71040f3407697a8d01deca40452940b75a8570b7
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-07-06 16:13:12 +0100

    Create custom fields to override the author of the content

--- partials/author-strap.php
+++ partials/author-strap.php
@@ -1,9 +1,34 @@
 <div class="author flex flex--align-center margin--bottom-default">
-    <div class="author__avatar">
-        <?php echo get_avatar( get_the_author_meta( 'ID' ), '60', $default, $alt, array( 'class' => array( 'border-radius--circle' ) ) ); ?>
-    </div>
-    <div class="author__meta margin--left-small">
-        <h4 class="margin--bottom-none">Created by <?php the_author_meta('display_name'); ?></h4>
-        <h5 class="margin--bottom-none text--grey-dark"><?php echo get_the_date( 'jS F' ); ?></h5>
-    </div>
+    <?php if( get_field('custom_author') != 'yes' ) { ?>
+        <div class="author__avatar">
+            <?php echo get_avatar( get_the_author_meta( 'ID' ), '60', $default, $alt, array( 'class' => array( 'border-radius--circle' ) ) ); ?>
+        </div>
+        <div class="author__meta margin--left-small">
+            <h4 class="margin--bottom-none">Created by <?php the_author_meta('display_name'); ?></h4>
+            <h5 class="margin--bottom-none text--grey-dark"><?php echo get_the_date( 'jS F' ); ?></h5>
+        </div>
+    <?php } else { ?>
+        <div class="author__avatar">
+            <?php
+            $image = get_field('author_avatar');
+
+            // vars
+            $url = $image['url'];
+            $title = $image['title'];
+            $alt = $image['alt'];
+            $caption = $image['caption'];
+
+            // thumbnail
+            $size = 'thumbnail';
+            $thumb = $image['sizes'][ $size ];
+            $width = '64px';
+            ?>
+
+            <img src="<?php echo $url; ?>" alt="<?php echo $alt; ?>" width="<?php echo $width; ?>" class="border-radius--circle">
+        </div>
+        <div class="author__meta margin--left-small">
+            <h4 class="margin--bottom-none">Created by <?php the_field('author_name')?></h4>
+            <h5 class="margin--bottom-none text--grey-dark"><?php echo get_the_date( 'jS F' ); ?></h5>
+        </div>
+    <?php } ?>
 </div>
