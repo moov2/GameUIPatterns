89573b2edd526645b8794b3703060a9016daab99
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-07-07 14:36:27 +0100

    Simplify rollovers

--- partials/gameui-examples.php
+++ partials/gameui-examples.php
@@ -11,6 +11,7 @@ if ( $connected->have_posts() ) :
     ?>
     <div class="padding--large">
         <h3>Game Examples:</h3>
+        <p>Tap on or hover over an example thumbnail to view the context of the component</p>
         <ul class="list examples">
             <?php while ( $connected->have_posts() ) : $connected->the_post(); ?>
                 <li class="examples__item">
@@ -19,9 +20,9 @@ if ( $connected->have_posts() ) :
                     <?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
                         <?php $featured_img_url = get_the_post_thumbnail_url($post->ID, 'examples__thumbnail'); ?>
                         <?php if(get_field('focus_image')) { ?>
-                            <img class="examples__thumbnail" src="<?php echo $featured_img_url; ?>" onmouseover="this.src='<?php echo get_field('focus_image'); ?>'" onmouseout="this.src='<?php echo $featured_img_url; ?>'">
+                            <img class="examples__thumbnail" src="<?php echo get_field('focus_image'); ?>" onmouseover="this.src='<?php echo $featured_img_url; ?>'" onmouseout="this.src='<?php echo get_field('focus_image'); ?>'">
                         <?php } else { ?>
-                            <img class="examples__thumbnail" src="<?php echo $featured_img_url; ?>" onmouseover="this.src='<?php echo $featured_img_url; ?>'" onmouseout="this.src='<?php echo $featured_img_url; ?>'">
+                            <img class="examples__thumbnail" src="<?php echo $featured_img_url; ?>">
                         <?php } ?>
                     <?php endif; ?>
 
