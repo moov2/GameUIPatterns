53922bb7d27049829f92b5515e4f42b35cd2bb28
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-07-07 16:58:30 +0100

    Lower amount of thumbnail resolutions to speed up WordPress

--- functions.php
+++ functions.php
@@ -16,27 +16,6 @@ if (function_exists('add_theme_support'))
     add_image_size('examples__thumbnail', 400, 300, true);
     add_image_size('wrapper', 850, '', true);
     add_image_size('wrapper-full', 1560, '', true);
-    add_image_size('large', 700, '', true); // Large Thumbnail
-    add_image_size('medium', 250, '', true); // Medium Thumbnail
-    add_image_size('small', 120, '', true); // Small Thumbnail
-    add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
-    // Add Support for Custom Backgrounds - Uncomment below if you're going to use
-    /*add_theme_support('custom-background', array(
-    'default-color' => 'FFF',
-    'default-image' => get_template_directory_uri() . '/img/bg.jpg'
-    ));*/
-    // Add Support for Custom Header - Uncomment below if you're going to use
-    /*add_theme_support('custom-header', array(
-    'default-image'          => get_template_directory_uri() . '/img/headers/default.jpg',
-    'header-text'            => false,
-    'default-text-color'     => '000',
-    'width'                  => 1000,
-    'height'                 => 198,
-    'random-default'         => false,
-    'wp-head-callback'       => $wphead_cb,
-    'admin-head-callback'    => $adminhead_cb,
-    'admin-preview-callback' => $adminpreview_cb
-    ));*/
     // Enables post and comment RSS feed links to head
     add_theme_support('automatic-feed-links');
     // Enable HTML5 support
