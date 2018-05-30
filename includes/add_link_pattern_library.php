358188eb3e27af31b59f7aa9a4c88a6e3210c9c8
mobinzk <mobinzk@gmail.com>
2018-05-30 12:50:25 +0100

    Fix url to Pattern library

--- includes/add_link_pattern_library.php
+++ includes/add_link_pattern_library.php
@@ -1,7 +1,7 @@
 <?php
 // Link to Pattern Library
 function register_my_custom_menu_page() {
-    add_menu_page( 'GameUIPatterns Pattern Library', 'Pattern Library', 'manage_options', '../../wp-content/themes/GameUIPatterns/pattern-library/dist/index.html', '', 'dashicons-book', 3 );
+    add_menu_page( 'GameUIPatterns Pattern Library', 'Pattern Library', 'manage_options', '../../app/themes/GameUIPatterns/pattern-library/dist/index.html', '', 'dashicons-book', 3 );
 }
 add_action( 'admin_menu', 'register_my_custom_menu_page' );
 ?>
